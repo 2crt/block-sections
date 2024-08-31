a7961c4e8c662a25e0fd4bab96f9a6aa8c4d19dc
emil <emil@htmlbox.net>
2024-08-31 14:34:27 +0300

    Obfuscate

--- src/register-wp-cli-commands.php
+++ src/register-wp-cli-commands.php
@@ -8,7 +8,7 @@ if ( ! class_exists( 'WP_CLI' ) ) {
 
 WP_CLI::add_command( 'make:section', MakeSectionCommand::class, [
 	'shortdesc' => 'Generate a section template',
-	'longdesc' => 'Generate a section template for 2create base WordPress theme based on ACF with InnerBlocks',
+	'longdesc' => 'Generate a section template for 2crt base WordPress theme based on ACF with InnerBlocks',
 	'synopsis' => [
 		'type' => 'assoc',
 		'name' => 'name',
