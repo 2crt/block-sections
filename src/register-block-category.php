b9b05016149730a5f01b46926eb2b47e9e9191a1
emil <emil@htmlbox.net>
2024-09-03 14:51:46 +0300

    Make "Theme Sections" definition first in the block inserter

--- src/register-block-category.php
+++ src/register-block-category.php
@@ -1,18 +1,12 @@
 <?php
 add_filter( 'block_categories_all' , function( $categories ) {
-    $new_categories = [];
-
-    // Add "Theme Sections" category, right before the "design" category
-    foreach ($categories as $category) {
-        if ($category['slug'] === 'design') {
-            $new_categories[] = [
-                'slug'  => 'app-block-sections',
-                'title' => 'Theme Sections'
-            ];
-        }
-
-        $new_categories[] = $category;
-    }
+    return [
+        [
+            'slug'  => 'app-block-sections',
+            'title' => 'Theme Sections'
+        ],
+        ...$categories
+    ];
 
 	return $new_categories;
 } );
